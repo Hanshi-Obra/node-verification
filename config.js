f01e60a7e29e603494ff2e4bd2bdf5b2371f9273
Hanshi-Obra <66540964+Hanshi-Obra@users.noreply.github.com>
2021-01-15 10:08:56 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const { ModelBuildPage } = require("twilio/lib/rest/autopilot/v1/assistant/modelBuild");
+
+module.exports = {
+    serviceID: "VAa3a5986bcb3b9db1500d1bacb73f4398",
+    accountSID: "AC7b648b683e507f4b2354c212de475429",
+    authToken: "6307384cc705f613bfe7e14fcded09dc",
+
+
+}
